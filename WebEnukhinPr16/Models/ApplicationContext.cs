ccf9fdbe3df663a4a6a2f12aaab45ac43d181f37
daddy <daddy@DESKTOP-UDFLTTO>
2022-05-22 16:22:14 +0300

    Добавьте файлы проекта.

---
+++ WebEnukhinPr16/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebEnukhinPr16.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
